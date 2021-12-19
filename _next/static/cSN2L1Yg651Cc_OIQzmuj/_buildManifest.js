cacf12a44ddb68efd4465f90f63654e83e1ff7eb
Amar <amar@crossml.com>
2021-12-19 21:58:20 +0530

    Updates

--- _next/static/cSN2L1Yg651Cc_OIQzmuj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-4b6b68dbf6c2d397ec8e.js"],"/_error":["static/chunks/pages/_error-b8622e7525c9b10836bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
